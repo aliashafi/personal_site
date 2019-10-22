72476c1e6a229ddf6cfbe2233bb4746ac509b75b
aliashafi <alia_shafi@alumni.brown.edu>
2019-10-22 14:16:32 -0700

    got the app to run

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require("path");
+const webpack = require('webpack');
+const SRC_DIR = path.join(__dirname, '/src');
+
 
 module.exports = {
     context: __dirname,
-    entry: "./entry_file.jsx",
+    entry: `${SRC_DIR}/entry_file.jsx`,
     output: {
-        path: path.join(__dirname, 'src'),
+        path: path.join(__dirname, "/dist"),
         filename: "bundle.js",
-        devtoolModuleFilenameTemplate: '[resourcePath]',
-        devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
     },
     module: {
-        module: {
             rules: [
                 {
                     test: /\.css$/,
@@ -29,7 +29,7 @@ module.exports = {
                     include: SRC_DIR,
                     loader: 'babel-loader',
                     query: {
-                        presets: ['react', 'es2015']
+                        presets: ['@babel/env', '@babel/react']
                     }
                 }
             ]
@@ -38,8 +38,7 @@ module.exports = {
             new webpack.DefinePlugin({
                 'process.env.NODE_ENV': JSON.stringify('production')
             })
-        ]
-    },
+        ],
     devtool: 'source-map',
     resolve: {
         extensions: ['.js', '.jsx', '*']
