2d7b8161099f66e6209e0373b2745be2796cd3e5
aliashafi <alia_shafi@alumni.brown.edu>
2019-11-13 14:43:11 -0800

    added the rest of my projects

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import summit from '../content/summit.jpg'
+import neuroDB from '../content/neuroDB.jpg'
+import lostInSpace from '../content/lostInSpace.jpg'
+import planet from '../content/planet.jpg'
+import neuroDB_logo from '../content/neuroDB_logo.jpg'
 import logoSummit from '../content/logo.jpg'
 import '../css/projects.css'
 class Projects extends React.Component {
@@ -28,6 +32,42 @@ class Projects extends React.Component {
             <div className="logo">
               <img src={logoSummit} alt="" />
             </div>
+            {/* <div className="tools-used">
+              <div>JavaScript</div>
+              <div>React/Redux</div>
+              <div>Ruby on Rails</div>
+              <div>postgreSQL</div>
+              <div>MapBox</div>
+              <div>Webpack</div>
+              <div>AWS</div>
+            </div> */}
+            <div className="project-links">
+              <a href="https://summit-fsp.herokuapp.com/"><i class="fas fa-share-square"></i></a>
+              <a href="https://github.com/aliashafi/summit"><i class="fab fa-github"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div className="projects-header"></div>
+        <div className="projects-container">
+          <div className="project-photo">
+            <img className="projects-summit" src={neuroDB} />
+          </div>
+
+          <div className="projects-about">
+            <div className="project-title-container"></div>
+            <div className="projects-caption"> NeuroDB is a database tool to help researchers studying 
+            intracranial Electrocorticography (ECoG) manage patient meta data and research related i
+            nformation. The project idea was inspired by the Chang Lab at UCSF, a neurosurgery lab in
+            terested in determining the basic mechanisms that underlie our ability to create and produce speech.
+            
+          </div>
+
+          </div>
+          <div className="logo-container">
+            <div className="logo">
+              <img src={neuroDB_logo} alt="" />
+            </div>
             <div className="project-links">
               <div>Live Link</div>
               <div>Github</div>
@@ -35,7 +75,37 @@ class Projects extends React.Component {
             </div>
           </div>
         </div>
+
+
+        <div className="projects-header"></div>
+        <div className="projects-container">
+          <div className="project-photo">
+            <img className="projects-summit" src={lostInSpace} />
+          </div>
+
+          <div className="projects-about">
+            <div className="project-title-container"></div>
+            <div className="projects-caption"> An interactive 3D game to explore the solar system. The application
+            was built using vanilla javascript and three.js 3D library. It incorporats three.js raycaster for collision detection​ and event listeners to allow for keyboard flight control.
+          </div>
+
+          </div>
+          <div className="logo-container">
+            
+            <div className="logo">
+              <img src={planet} alt="" />
+            </div>
+            <div className="project-links">
+              <div>Live Link</div>
+              <a href="https://github.com/aliashafi/summit"><i class="fab fa-github"></i></a>
+              <div>More About the Project</div>
+            </div>
+          </div>
+        </div>
       </div>
+
+
+      
     )
   }
 
